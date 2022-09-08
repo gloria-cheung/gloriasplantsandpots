dfa3875ffd4fcb8b3a36c88a08a16852bd8816b7
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-08 14:43:06 -0400

    add routes for orders and categories

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
+  resources :categories, only: [:show]
+  resources :orders, only: [:create, :show]
 
 end

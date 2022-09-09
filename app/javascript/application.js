c0a2c991f34172e3436223388f0ba7facca34059
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-09-08 21:51:39 -0400

    import popper and bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
-import "bootstrap";
-//= require material
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap-sprockets
-//= require_tree .
+import "./popper";
+import "./bootstrap.min";

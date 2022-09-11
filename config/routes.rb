b1935fda5cbcdfb4dd775c160897f415f2606600
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-09-10 20:09:08 -0400

    add index route for categories

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
-  resources :categories, only: [:show]
+  resources :categories, only: [:index, :show]
   resources :orders, only: [:create, :show]
 
 end

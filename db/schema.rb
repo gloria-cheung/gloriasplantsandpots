8cb5b61d8be5d7227ffc136808dadaca72465307
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-09-11 14:16:35 -0400

    run line item migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_11_154555) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_11_181317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_11_154555) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "item_price_cents"
+    t.integer "total_price_cents"
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_line_items_on_order_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -41,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_11_154555) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
 end

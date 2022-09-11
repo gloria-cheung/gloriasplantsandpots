1e583205b7f5448a545b2da94b38ba6133d09ff4
gloria <macuser@MacUsers-MBP.JOOWIN>
2022-09-11 16:05:59 -0400

    add route to about page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: 'products#index'
 
+  get '/about' => 'about#index'
+
   resources :products, only: [:index, :show]
 
   resources :categories, only: [:index, :show]
